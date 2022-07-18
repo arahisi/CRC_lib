ba0a8217306f5f703ee26ac2aa81fd6cc332ba7e
Shoji Yamamoto <67620443+arahisi@users.noreply.github.com>
2022-07-18 11:35:53 +0900

    Update crc_lib.h

--- crc_lib.h
+++ crc_lib.h
@@ -1,23 +1,23 @@
 /*
-Copyright 2021, Shoji Yamamoto
+ Copyright 2021, Shoji Yamamoto
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to deal 
-in the Software without restriction, including without limitation the rights 
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
-of the Software, and to permit persons to whom the Software is furnished to do 
-so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all 
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-DEALINGS IN THE SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+ */
 
 #ifndef _CRC_LIB_H_
 #define _CRC_LIB_H_
@@ -29,22 +29,22 @@ extern "C" {
 #include <stdio.h>
 #include <stdint.h>
 
-#define CRC_LIB_DEFINE_SET(CRCTYPE)\
+#define CRC_LIB_DEFINE_TABLESET(CRCTYPE)\
 typedef struct {\
 	CRCTYPE Table[256];\
 	CRCTYPE Initial;\
-} CRCTYPE##_SET;
+} CRC_LIB_##CRCTYPE##_TABLESET;
 
 #if _CRC_LIB_
 #define CRC_LIB_DEFINE_FUNCTION(CRCTYPE)\
-static void CRCTYPE##_GenerateTable(CRCTYPE table[256], CRCTYPE ipolynominal) {\
+static void CRC_LIB_##CRCTYPE##_GenerateTable(CRCTYPE table[256], CRCTYPE ipolynominal) {\
 	int dividend;\
 	CRCTYPE remainder;\
-	uint8_t bit;\
+    int bit;\
 	for (dividend = 0; dividend < 256; dividend++) {\
 		remainder = dividend;\
-		for (bit = 8; bit > 0; bit--) {\
-			if (remainder & 1) {\
+		for (bit = (8 * sizeof(CRCTYPE)); bit > 0; bit--) {\
+			if (remainder > 0) {\
 				remainder = (remainder >> 1) ^ ipolynominal;\
 			} else {\
 				remainder = (remainder >> 1);\
@@ -53,41 +53,49 @@ static void CRCTYPE##_GenerateTable(CRCTYPE table[256], CRCTYPE ipolynominal) {\
 		table[dividend] = remainder;\
 	}\
 }\
-void CRCTYPE##_InitializeSET(CRCTYPE##_SET* set, CRCTYPE ipolynominal, CRCTYPE initial) {\
-	CRCTYPE##_GenerateTable(set->Table, ipolynominal);\
+void CRC_LIB_##CRCTYPE##_Initialize(CRC_LIB_##CRCTYPE##_TABLESET* set, CRCTYPE ipolynominal, CRCTYPE initial) {\
+	CRC_LIB_##CRCTYPE##_GenerateTable(set->Table, ipolynominal);\
 	set->Initial = initial;\
 }\
-CRCTYPE CRCTYPE##_CRC(const CRCTYPE##_SET* set, uint8_t byte, CRCTYPE crc) {\
-	return crc = set->Table[crc ^ byte];\
+CRCTYPE CRC_LIB_##CRCTYPE##_CRC(const CRC_LIB_##CRCTYPE##_TABLESET* set, const void* data, CRCTYPE crc) {\
+	crc = set->Table[0xFF & (crc ^ ((CRCTYPE)(*((const uint8_t*)data))))] ^ (crc >> 8);\
+	return crc;\
 }\
-CRCTYPE CRCTYPE##_ContinueCRC(const CRCTYPE##_SET* set, const void* data, size_t size, CRCTYPE crc) {\
+CRCTYPE CRC_LIB_##CRCTYPE##_ContinueCRC(const CRC_LIB_##CRCTYPE##_TABLESET* set, const void* data, size_t size, CRCTYPE crc) {\
 	const uint8_t *p = (const uint8_t*) data;\
 	while (size) {\
-		crc = set->Table[crc ^ *p];\
+		crc = set->Table[0xFF & (crc ^ ((CRCTYPE)(*p)))] ^ (crc >> 8);\
 		p++;\
 		size--;\
 	}\
 	return crc;\
 }\
-CRCTYPE CRCTYPE##_GetCRC(const CRCTYPE##_SET* set, const void* data, size_t size) {\
-	return CRCTYPE##_ContinueCRC(set, data, size, set->Initial);\
+CRCTYPE CRC_LIB_##CRCTYPE##_GetCRC(const CRC_LIB_##CRCTYPE##_TABLESET* set, const void* data, size_t size) {\
+	return CRC_LIB_##CRCTYPE##_ContinueCRC(set, data, size, set->Initial);\
 }
 #else
 #define CRC_LIB_DEFINE_FUNCTION(CRCTYPE)\
-extern void CRCTYPE##_InitializeSET(CRCTYPE##_SET* set, CRCTYPE ipolynominal, CRCTYPE initial);\
-extern CRCTYPE CRCTYPE##_CRC(const CRCTYPE##_SET* set, uint8_t byte, CRCTYPE crc);\
-extern CRCTYPE CRCTYPE##_ContinueCRC(const CRCTYPE##_SET* set, const void* data, size_t size, CRCTYPE crc);\
-extern CRCTYPE CRCTYPE##_GetCRC(const CRCTYPE##_SET* set, const void* data, size_t size);
+	extern void CRC_LIB_##CRCTYPE##_Initialize(CRC_LIB_##CRCTYPE##_TABLESET* set, CRCTYPE ipolynominal, CRCTYPE initial);\
+	extern CRCTYPE CRC_LIB_##CRCTYPE##_CRC(const CRC_LIB_##CRCTYPE##_TABLESET* set, const void* data,  CRCTYPE crc);\
+	extern CRCTYPE CRC_LIB_##CRCTYPE##_ContinueCRC(const CRC_LIB_##CRCTYPE##_TABLESET* set, const void* data, size_t size, CRCTYPE crc);\
+	extern CRCTYPE CRC_LIB_##CRCTYPE##_GetCRC(const CRC_LIB_##CRCTYPE##_TABLESET* set, const void* data, size_t size);
 #endif
 
-CRC_LIB_DEFINE_SET(uint8_t)
-CRC_LIB_DEFINE_SET(uint16_t)
-CRC_LIB_DEFINE_SET(uint32_t)
+CRC_LIB_DEFINE_TABLESET(uint8_t)
+CRC_LIB_DEFINE_TABLESET(uint16_t)
+CRC_LIB_DEFINE_TABLESET(uint32_t)
 
 CRC_LIB_DEFINE_FUNCTION(uint8_t)
 CRC_LIB_DEFINE_FUNCTION(uint16_t)
 CRC_LIB_DEFINE_FUNCTION(uint32_t)
 
+
+#define CRC_LIB_TABLESET(CRCTYPE) CRC_LIB_##CRCTYPE##_TABLESET
+#define CRC_LIB_Initialize(CRCTYPE, set, ipolynominal, initial) CRC_LIB_##CRCTYPE##_Initialize(set, ipolynominal, initial)
+#define CRC_LIB_CRC(CRCTYPE, set, dat, crc) CRC_LIB_##CRCTYPE##_CRC(set, data, crc)
+#define CRC_LIB_ContinueCRC(CRCTYPE, set, data, size, crc) CRCTYPE CRC_LIB_##CRCTYPE##_ContinueCRC(set, data, size, crc)
+#define CRC_LIB_GetCRC(CRCTYPE, set, data, size) CRC_LIB_##CRCTYPE##_GetCRC(set, data, size)
+#
 #ifdef __cplusplus
 }
 #endif
