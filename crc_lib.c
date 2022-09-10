0a3cadb9bd995287608e7a7410e90e60da646ce7
Shoji Yamamoto <greatshoji@yahoo.co.jp>
2022-09-10 10:59:49 +0900

    update

--- crc_lib.c
+++ crc_lib.c
@@ -23,3 +23,27 @@ DEALINGS IN THE SOFTWARE.
 #include "crc_lib.h"
 #undef _CRC_LIB_
 #define _CRC_LIB_ 0
+
+uint8_t CRC_LIB_uint8_t_Reflect(uint8_t x) {
+	x = ((x & 0x55) << 1) | ((x & 0xAA) >> 1);
+	x = ((x & 0x33) << 2) | ((x & 0xCC) >> 2);
+	x = (x << 4) | (x >> 4);
+	return x;
+}
+
+uint16_t CRC_LIB_uint16_t_Reflect(uint16_t x) {
+	x = ((x & 0x5555) << 1) | ((x & 0xAAAA) >> 1);
+	x = ((x & 0x3333) << 2) | ((x & 0xCCCC) >> 2);
+	x = ((x & 0x0F0F) << 4) | ((x & 0xF0F0) >> 4);
+	x = (x << 8) | (x >> 8);
+	return x;
+}
+
+uint32_t CRC_LIB_uint32_t_Reflect(uint32_t x) {
+	x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1);
+	x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2);
+	x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4);
+	x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8);
+	x = (x << 16) | (x >> 16);
+	return x;
+}
